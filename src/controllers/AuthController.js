68da560471ebb2c13c345b829da2a59a0066338d
vishalVivahSahyog <vishalshrivastava914419@gmail.com>
2025-02-07 20:09:17 +0530

    first commit

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,14 @@
+const authController = () => {
+    const login = (req, res) => {
+        // login logic
+    };
+    
+    const register = (req, res) => {
+        // register logic
+    };
+    
+    return {
+        login,
+        register
+    };
+}
